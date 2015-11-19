6d604b07125997250ae4effb18dde35dede0ba6f
almaskz <almas.kairatuly@gmail.com>
2015-11-19 19:46:30 +0600

    Initial Commit

---
+++ Food Delivery KTZ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Food Delivery KTZ
+//
+//  Created by Almas Kairatuly on 11/19/15.
+//  Copyright © 2015 Almas Kairatuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Food Delivery KTZ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Food Delivery KTZ
+//
+//  Created by Almas Kairatuly on 11/19/15.
+//  Copyright © 2015 Almas Kairatuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
